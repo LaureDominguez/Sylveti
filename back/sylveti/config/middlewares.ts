fc0d34a8a3c7aca55d2b3e65f22aa6885b4090ce
Laure Dominguez <dominguezlaure@gmail.com>
2025-09-26 16:45:56 +0200

    Initial commit

---
+++ back/sylveti/config/middlewares.ts
@@ -0,0 +1,19 @@
+export default [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['http://localhost:3000', 'https://sylveti.com'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
